5c01efaf664a9db8820ae90ae40d9c991e59b4fa
Alexandre Carvalho <ahcarvalho01@gmail.com>
2016-10-22 14:12:05 -0200

    Modelando classes para realizar operações

--- src/br/ufla/dcc/ppoo/modelo/Filme.java
+++ src/br/ufla/dcc/ppoo/modelo/Filme.java
@@ -14,7 +14,7 @@ public class Filme {
     /*Criando atributos da classe, nome das variaveis e conteudo da janela de
       conteúdo da janela de filmes.
     */
-    private double cod;
+    private double cod ;
     private String nome;
     private String genero;
     private int ano;
@@ -25,8 +25,9 @@ public class Filme {
      * @return the nome
      */
 
-    public Filme(double cod, String nome, String genero, int ano, String duracao, String descricao){
-      this.cod = cod;
+    public Filme(String nome, String genero, int ano, String duracao, String descricao){
+      //pensando na lógica de auto increment para o cod+1 a cada new Filmes
+      //this.cod +=1;
       this.nome = nome;
       this.genero = genero;
       this.ano = ano;
